cf21ea71d1e3449d62b1ca41d69ec724c6142fe5
iizaghz <sukmahawaiza@gmail.com>
2026-04-15 02:30:14 +0700

    add form input konsumsi

--- SistemNutrisi/FormKonsumsi.Designer.cs
+++ SistemNutrisi/FormKonsumsi.Designer.cs
@@ -2,6 +2,123 @@ namespace SistemNutrisi
 {
     partial class FormKonsumsi
     {
-       
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMakanan = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpTanggal = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(400, 60);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INPUT KONSUMSI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.Location = new System.Drawing.Point(30, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Makanan";
+            // 
+            // cmbMakanan
+            // 
+            this.cmbMakanan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMakanan.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbMakanan.FormattingEnabled = true;
+            this.cmbMakanan.Location = new System.Drawing.Point(140, 82);
+            this.cmbMakanan.Name = "cmbMakanan";
+            this.cmbMakanan.Size = new System.Drawing.Size(230, 31);
+            this.cmbMakanan.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(30, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tanggal";
+            // 
+            // dtpTanggal
+            // 
+            this.dtpTanggal.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.dtpTanggal.Location = new System.Drawing.Point(140, 122);
+            this.dtpTanggal.Name = "dtpTanggal";
+            this.dtpTanggal.Size = new System.Drawing.Size(230, 30);
+            this.dtpTanggal.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label4.Location = new System.Drawing.Point(30, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Jumlah Porsi";
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtJumlah.Location = new System.Drawing.Point(140, 162);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(230, 30);
+            this.txtJumlah.TabIndex = 6;
+            
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMakanan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpTanggal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnBack;
     }
 }
