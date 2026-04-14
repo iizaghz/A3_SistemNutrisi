adb38985bc32417465cdd7c6bce8082238f1d5c8
iizaghz <sukmahawaiza@gmail.com>
2026-04-15 02:28:39 +0700

    add header dan input kategori

--- SistemNutrisi/FormKategori.Designer.cs
+++ SistemNutrisi/FormKategori.Designer.cs
@@ -2,6 +2,123 @@ namespace SistemNutrisi
 {
     partial class FormKategori
     {
-        
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlInput = new System.Windows.Forms.Panel();
+
+            this.txtNamaKategori = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlActions = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlHeader.SuspendLayout();
+            this.pnlInput.SuspendLayout();
+            this.pnlActions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.pnlHeader.Controls.Add(this.btnBack);
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(920, 74);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(16, 18);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(100, 37);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(400, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategori Makanan";
+            // 
+            // pnlInput
+            // 
+            this.pnlInput.Controls.Add(this.txtNamaKategori);
+            this.pnlInput.Controls.Add(this.label3);
+            this.pnlInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlInput.Location = new System.Drawing.Point(0, 74);
+            this.pnlInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlInput.Name = "pnlInput";
+            this.pnlInput.Padding = new System.Windows.Forms.Padding(27, 25, 27, 25);
+            this.pnlInput.Size = new System.Drawing.Size(920, 85);
+            this.pnlInput.TabIndex = 1;
+            // 
+
+            // 
+            // txtNamaKategori
+            // 
+            this.txtNamaKategori.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtNamaKategori.Location = new System.Drawing.Point(173, 25);
+            this.txtNamaKategori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNamaKategori.Name = "txtNamaKategori";
+            this.txtNamaKategori.Size = new System.Drawing.Size(332, 30);
+            this.txtNamaKategori.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(31, 28);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nama Kategori:";
+            
+        }
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlInput;
+        private System.Windows.Forms.TextBox txtNamaKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel pnlActions;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
