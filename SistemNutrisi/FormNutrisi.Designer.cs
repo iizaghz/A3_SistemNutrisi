ae4c97a7327c932678782803eac738f57c22789a
iizaghz <sukmahawaiza@gmail.com>
2026-04-15 02:33:23 +0700

    add form input nutrisi

--- SistemNutrisi/FormNutrisi.Designer.cs
+++ SistemNutrisi/FormNutrisi.Designer.cs
@@ -2,6 +2,134 @@ namespace SistemNutrisi
 {
     partial class FormNutrisi
     {
-       
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlInput = new System.Windows.Forms.Panel();
+            this.txtKarbohidrat = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtLemak = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtProtein = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtKalori = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbMakanan = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlActions = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlHeader.SuspendLayout();
+            this.pnlInput.SuspendLayout();
+            this.pnlActions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.pnlHeader.Controls.Add(this.btnBack);
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1200, 74);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(16, 18);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(100, 37);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(507, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Nutrisi";
+            // 
+            // pnlInput
+            // 
+            this.pnlInput.Controls.Add(this.txtKarbohidrat);
+            this.pnlInput.Controls.Add(this.label8);
+            this.pnlInput.Controls.Add(this.txtLemak);
+            this.pnlInput.Controls.Add(this.label7);
+            this.pnlInput.Controls.Add(this.txtProtein);
+            this.pnlInput.Controls.Add(this.label6);
+            this.pnlInput.Controls.Add(this.txtKalori);
+            this.pnlInput.Controls.Add(this.label5);
+            this.pnlInput.Controls.Add(this.cmbMakanan);
+            this.pnlInput.Controls.Add(this.label3);
+            this.pnlInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlInput.Location = new System.Drawing.Point(0, 74);
+            this.pnlInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlInput.Name = "pnlInput";
+            this.pnlInput.Padding = new System.Windows.Forms.Padding(27, 25, 27, 25);
+            this.pnlInput.Size = new System.Drawing.Size(1200, 185);
+            this.pnlInput.TabIndex = 1;
+            // 
+            // txtKarbohidrat
+            // 
+            this.txtKarbohidrat.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtKarbohidrat.Location = new System.Drawing.Point(720, 111);
+            this.txtKarbohidrat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKarbohidrat.Name = "txtKarbohidrat";
+            this.txtKarbohidrat.Size = new System.Drawing.Size(199, 30);
+            this.txtKarbohidrat.TabIndex = 9;
+            
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbMakanan;
+        private System.Windows.Forms.TextBox txtKarbohidrat;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtLemak;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtProtein;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKalori;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel pnlActions;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
