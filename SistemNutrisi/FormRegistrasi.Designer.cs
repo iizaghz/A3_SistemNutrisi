51ce280bef0ef7557cb4c58530b602ec1bbf3542
iizaghz <sukmahawaiza@gmail.com>
2026-04-15 02:22:29 +0700

    add form input registrasi

--- SistemNutrisi/FormRegistrasi.Designer.cs
+++ SistemNutrisi/FormRegistrasi.Designer.cs
@@ -48,8 +48,62 @@ namespace SistemNutrisi
             this.label1.TabIndex = 0;
             this.label1.Text = "REGISTRASI AKUN";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.Location = new System.Drawing.Point(36, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Lengkap";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtNama.Location = new System.Drawing.Point(36, 122);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(360, 32);
+            this.txtNama.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(36, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtEmail.Location = new System.Drawing.Point(36, 187);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(360, 32);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label4.Location = new System.Drawing.Point(36, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtPassword.Location = new System.Drawing.Point(36, 252);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.Size = new System.Drawing.Size(360, 32);
+            this.txtPassword.TabIndex = 6;
             
-
         }
         #endregion
 
